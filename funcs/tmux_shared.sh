d27065422f65e2ba2eb06ecafabd8f0377b9fdd5
cwshugg <cwshugg@vt.edu>
2021-01-15 13:24:03 -0500

    added a small script to create shared tmux sessions

---
+++ funcs/tmux_shared.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Creates a new shared tmux session with a random name. The socket location
+# and name can be specified to replace the default.
+#
+#   Connor Shugg
+
+# main function
+function __shuggtool_tmux_shared_new()
+{
+    # generate a default name for the shared socket
+    socket=$(whoami)_tmux_shared_socket
+    junk_length=10
+    junk=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c $junk_length)
+    socket=${socket}_${junk}
+
+    # check for a command-line argument
+    if [ $# -ge 1 ]; then
+        socket=$1
+    fi
+
+    # attempt to create the tmux session in /tmp
+    # ('-d' tells tmux to start the session detatched)
+    tmux -S /tmp/$socket new-session -d -s $socket
+    
+    # send a command up to print out info for the onlookers
+    comm1="clear; clear; echo 'Onlookers can attach via:';"
+    comm2="echo '-------------------------';"
+    comm3="echo 'tmux -S /tmp/$socket attach-session -t $socket';"
+    comm4="echo '-------------------------';"
+    comm5="echo '(Add -r to make it read-only)';"
+    tmux -S /tmp/$socket send-keys -t $socket \
+         "$comm1 $comm2 $comm3 $comm4 $comm5 " ENTER
+
+    # update the permissions on the socket to allow onlookers
+    chmod 777 /tmp/$socket
+
+    # attach to the socket
+    tmux -S /tmp/$socket attach-session -t $socket
+}
+
+# pass all args to main function
+__shuggtool_tmux_shared_new "$@"
+
