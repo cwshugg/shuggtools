94f0c003bca374d314e402c1af8187e87c007000
cwshugg <cwshugg@vt.edu>
2021-05-19 17:17:27 -0400

    updated aliases.sh's second fancy prompt, and removed the suffix

--- aliases.sh
+++ aliases.sh
@@ -25,8 +25,8 @@ pc6="40;75;163"     # light blue
 pc_black="0;0;0"    # black
 pc_white="255;255;255" # white
 pc_none="0"         # none
-p_suffix="$(echo -e '\u2596')" #"▖"
-b_line="$(echo -e '\u2500')"
+p_suffix="" #"$(echo -e '\u2596')"
+b_line="$(echo -e '\u2501')"
 b_fork="$(echo -e '\u2533')"
 b_corner="$(echo -e '\u2517')"
 
