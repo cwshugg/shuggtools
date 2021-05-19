ee7fa0aa4196b2defb933c554ae96e142170607e
Connor Shugg <t-cshugg@microsoft.com>
2021-05-19 15:50:02 -0400

    added cool prompt

--- aliases.sh
+++ aliases.sh
@@ -9,19 +9,39 @@ alias valgfull="valgrind -v --leak-check=full --show-leak-kinds=all --track-orig
 # other alises
 alias dir="ls"
 
-# fancy colored prompt
+
+# ------------------------------- bash prompt ------------------------------- #
+ptoggle=1
+
+# fancy prompt variables
 bgc="48;2"          # prefix for background colors
 fgc="38;2"          # prefix for foreground colors
-pc1="210;129;7"     # yellow    (235, 154,  32)
-pc2="180;17;33"     # red       (205,  42,  58)
-pc3="0;30;128"      # blue      ( 20,  55, 143)
+pc1="210;129;7"     # dark yellow   (original: (235, 154,  32))
+pc2="180;17;33"     # dark red      (original: (205,  42,  58))
+pc3="0;30;128"      # dark blue     (original: ( 20,  55, 143))
+pc4="255;174;52"    # light yellow
+pc5="225;62;78"     # light red
+pc6="40;75;163"     # light blue
 pc_black="0;0;0"    # black
 pc_white="255;255;255" # white
 pc_none="0"         # none
 p_suffix="$(echo -e '\u2596')" #"▖"
-PS1="\[\033[${bgc};${pc3};${fgc};${pc_white}m\] \u \[\033[${bgc};${pc2};${fgc};${pc_black}m\] \h \[\033[${bgc};${pc1};${fgc};${pc_black}m\] \W \[\033[${pc_none}m\]\[\033[${fgc};${pc1}m\]${p_suffix}\[\033[${pc_none}m\] "
+b_line="$(echo -e '\u2500')"
+b_fork="$(echo -e '\u2533')"
+b_corner="$(echo -e '\u2517')"
 
-# standard colored prompt
-#PS1="[\[\033[01;32m\]\u\[\033[00m\]@\[\033[01;31m\]\h\[\033[00m\]: \[\033[01;36m\]\W\[\033[00m\]] ";
-# colorless prompt
-#PS1="[\! \u@\h: \W] "
+if [ $ptoggle -eq 0 ]; then
+    # fancy prompt 1
+    PS1="\[\033[${bgc};${pc3};${fgc};${pc_white}m\] \u \[\033[${bgc};${pc2};${fgc};${pc_white}m\] \h \[\033[${bgc};${pc1};${fgc};${pc_black}m\] \W \[\033[${pc_none}m\]\[\033[${fgc};${pc1}m\]${p_suffix}\[\033[${pc_none}m\] "
+elif [ $ptoggle -eq 1 ]; then
+    # fancy prompt 2
+    PS1="\[\033[${bgc};${pc3};${fgc};${pc_white}m\] \u \[\033[${pc_none}m\]${b_line}${b_fork}${b_line}\[\033[${bgc};${pc2};${fgc};${pc_white}m\] \h \[\033[${bgc};${pc1};${fgc};${pc_black}m\] \W \[\033[${pc_none}m\]\[\033[${fgc};${pc1}m\]${p_suffix}\[\033[${pc_none}m\]\n          ${b_corner}${b_line} "
+else
+    # standard colored prompt
+    PS1="[\[\033[${fgc};${pc6}m\]\u\[\033[${pc_none}m\]@\[\033[${fgc};${pc5}m\]\h\[\033[${pc_none}m\]: \[\033[${fgc};${pc4}m\]\W\[\033[${pc_none}m\]] "
+    # colorless prompt
+    #PS1="[\! \u@\h: \W] "
+fi
+
+# --------------------------------------------------------------------------- #
+
