a38069bec2d1230dd2d72079a3b78b297a04ced3
cwshugg <cwshugg@vt.edu>
2021-10-05 16:05:17 -0400

    added color schemes to tmux

--- funcs/shugg_tmux_config.sh
+++ funcs/shugg_tmux_config.sh
@@ -8,10 +8,31 @@ function __shuggtool_tmux_config()
     config_location=~/.tmux.conf
     echo -e "Writing to ${c_green}${config_location}${c_none}..."
     
+    # based on the username, decide what color to use
+    status_bg_color="colour18"  # dark blue
+    status_fg_color="colour172" # yellow
+    pane_active_color="${status_fg_color}"
+    pane_inactive_color="${status_bg_color}"
+
+    # special case for root
+    username=$(whoami)
+    if [[ "$username" == "root" ]]; then
+        status_bg_color="colour235" # dark gray
+        status_fg_color="colour46"  # green
+        pane_active_color="${status_fg_color}"
+        pane_inactive_color="${status_bg_color}"
+    fi
+
     # custom tmux settings
-    echo "# Connor's tmux settings" > $config_location
-    echo "# enable tmux window colors" >> $config_location
-    echo "set -g default-terminal \"screen-256color\"" >> $config_location
+    echo "# Connor's tmux settings"                     > $config_location
+    echo "# enable tmux window colors"                  >> $config_location
+    echo "set -g default-terminal \"screen-256color\""  >> $config_location
+    echo "# set status bar color"                       >> $config_location
+    echo "set -g status-bg ${status_bg_color}"          >> $config_location
+    echo "set -g status-fg ${status_fg_color}"          >> $config_location
+    echo "# set pane divider colors"                    >> $config_location
+    echo "set -g pane-border-style fg=${pane_inactive_color}" >> $config_location
+    echo "set -g pane-active-border-style fg=${pane_active_color}" >> $config_location
 
     echo -e "${c_green}${config_location}${c_none} written successfully."
 }
