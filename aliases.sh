59ef9a1eb8493949f104bda7b283ced40d7824dd
cwshugg <connor@shugg.dev>
2023-01-26 09:22:58 -0500

    added 'bat' alias

--- aliases.sh
+++ aliases.sh
@@ -17,6 +17,16 @@ if [ ! -z "${git}" ]; then
     ${git} config --global core.editor "'$(which vim)'"
 fi
 
+# alias 'bat' or 'batcat' to 'cat' (syntax-highlighting!)
+bat_binary="$(which batcat 2> /dev/null)"
+if [ -z "${bat_binary}" ]; then
+    bat_binary="$(which bat 2> /dev/null)"
+fi
+if [ ! -z "${bat_binary}" ]; then
+    alias cat="${bat_binary} --paging=never"
+    alias pcat="${bat_binary}"
+fi
+
 # coding/debugging aliases
 alias valgfull="valgrind -v --leak-check=full --show-leak-kinds=all --track-origins=yes"
 alias gdb="gdb -q" # quiet-mode GDB (don't print intro)
