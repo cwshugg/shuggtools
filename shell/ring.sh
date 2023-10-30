ff4b6befc57e675a007f79091918ca31c829ccc0
cwshugg <connor@shugg.dev>
2023-10-30 13:42:42 -0400

    added extra options to ring

--- shell/ring.sh
+++ shell/ring.sh
@@ -14,6 +14,8 @@ function __shuggtool_ring_usage()
     echo "------------------------------------------------------------------------"
     echo " -h               Shows this help menu"
     echo " -r RATE_SECONDS  Specifies the number of seconds to wait between bells."
+    echo " -t TEXT          Specifies the text to display when ringing."
+    echo " -q               Enables \"quiet mode\", which disables text printing."
     echo "------------------------------------------------------------------------"
 }
 
@@ -21,10 +23,12 @@ function __shuggtool_ring_usage()
 function __shuggtool_ring()
 {
     bell_rate=1
+    bell_text="ringing..."
+    bell_quiet=0
     
     # first, check for command-line arguments
     local OPTIND h c t
-    while getopts "hr:" opt; do
+    while getopts "hr:t:q" opt; do
         case ${opt} in
             h)
                 __shuggtool_ring_usage
@@ -38,16 +42,44 @@ function __shuggtool_ring()
                     return 1
                 fi
                 ;;
+            t)
+                bell_text="${OPTARG}"
+                ;;
+            q)
+                bell_quiet=1
+                ;;
             *)
                 __shuggtool_ring_usage
                 return 1
                 ;;
         esac
     done
+
+    # set up an array of colors to use
+    bell_colors=("${C_WHITE}" "${C_RED}" "${C_GREEN}" "${C_YELLOW}" "${C_BLUE}"
+                 "${C_PURPLE}" "${C_CYAN}" "${C_LTRED}" "${C_LTGREEN}"
+                 "${C_LTYELLOW}" "${C_LTBLUE}" "${C_LTPURPLE}" "${C_LTCYAN}")
+    bell_colors_len=${#bell_colors[@]}
     
     # repeatedly bell
+    c_idx_last=-1
     while true; do
-        echo -en "\a"
+        if [ ${bell_quiet} -eq 0 ]; then
+            # select a random color (make sure it's different than the last one)
+            c_idx=$((${RANDOM} % bell_colors_len))
+            if [ ${c_idx} -eq ${c_idx_last} ]; then
+                c_idx=$(((c_idx + 1) % bell_colors_len))
+            fi
+            c_idx_last=${c_idx}
+            c="${bell_colors[${c_idx}]}"
+    
+            # output the text and the bell
+            echo -en "\r${c}${bell_text}${C_NONE}\a"
+        else
+            echo -en "\a"
+        fi
+
+        # sleep until the next one
         sleep ${bell_rate}
     done
 }
